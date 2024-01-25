161ef519783e8135bfc3c859d601a3d5a2c52234
Karoll Escalante <karollescalante02@gmail.com>
2024-01-24 23:31:26 -0500

    style: format imports and functions in main.py
    Refactor the code in main.py to follow PEP 8 style guidelines. This includes organizing and formatting imports and functions for better readability and consistency.

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+from bs4 import BeautifulSoup
 from simplegmail import Gmail
 from simplegmail.query import construct_query
-from bs4 import BeautifulSoup
+
 
 def extract_url_from_html(html_body):
     """Extracts the URL from HTML body."""
     try:
-        soup = BeautifulSoup(html_body, 'html.parser')
-        link_tag = soup.find('a', href=lambda value: value and value.startswith('https://view.connect.etoro.com/'))
-        return link_tag.get('href') if link_tag else None
+        soup = BeautifulSoup(html_body, "html.parser")
+        link_tag = soup.find(
+            "a",
+            href=lambda value: value
+            and value.startswith("https://view.connect.etoro.com/"),
+        )
+        return link_tag.get("href") if link_tag else None
     except Exception as e:
         print(f"Error extracting URL: {e}")
         return None
+
 
 def main():
     try:
@@ -31,11 +37,11 @@ def main():
             if url:
                 print(url)
             else:
-                print('No matching <a> tag found with the specified URL.')
+                print("No matching <a> tag found with the specified URL.")
 
     except Exception as e:
         print(f"Error: {e}")
 
+
 if __name__ == "__main__":
     main()
-
